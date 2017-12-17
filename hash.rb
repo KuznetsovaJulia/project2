9dd809f9fb615ca960aa6b0abda03f72733a3ea7
Vladik <vlad1997kov@gmail.com>
2017-12-17 16:00:52 +0300

    some formating and variables delete

--- hash.rb
+++ hash.rb
@@ -1,14 +1,11 @@
 require 'prime'
-hashh = {'ddd' => 4, 'JJJ' => 2, 'h' => 7}
-l='odd'
-def hash_p(hash,s)
-  case s
+def hash_p(hash, string = 'even')
+  case string
     when 'prime'
-    result=hash.find_all{ |key, value| Prime.prime?(value)}
+      result = hash.find_all{ |key, value| Prime.prime?(value)}
     when 'odd'
-      result=hash.find_all{ |key, value| value % 2== 1}
+      result = hash.find_all{ |key, value| value % 2 == 1}
     when 'even'
-      result=hash.find_all{ |key, value| value % 2== 0}
+      result = hash.find_all{ |key, value| value % 2 == 0}
   end
 end
-puts hash_p(hashh,l)
